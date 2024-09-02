8af6448f18569bfd19a9f6032c7f83c42e4d0543
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2024-09-02 23:52:28 +0530

    initial dev setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
+import { Toaster } from "@/components/ui/toaster"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <><Component {...pageProps} /><Toaster /></>;
 }
